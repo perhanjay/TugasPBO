1d571621ff51f91d9b91215f7b4e0a6bd09346ad
neiveIsAfraid <muhammad.iqbal.ferhan@gmail.com>
2025-03-10 22:29:33 +0800

    Changing the filename and formula for calculating volume of a tabung

---
+++ src/Modul1/VolumeTabung.java
@@ -0,0 +1,20 @@
+package Modul1;
+
+public class VolumeTabung {
+    double radius;
+    double height;
+
+    public VolumeTabung(double range, double height) {
+        this.radius = range;
+        this.height = height;
+    }
+
+    double hitungVolume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    public static void main(String[] args) {
+        VolumeTabung tabung = new VolumeTabung(5, 10);
+        System.out.println(tabung.hitungVolume());
+    }
+}
